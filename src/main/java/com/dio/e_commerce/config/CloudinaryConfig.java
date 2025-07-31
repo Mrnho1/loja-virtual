a004814a8a433e61b67815d905f6b5db8a23497d
lucas <ls.marinho36@gmail.com>
2025-07-31 10:58:15 -0300

    update(all): cloudnary inclusion

--- src/main/java/com/dio/e_commerce/config/CloudinaryConfig.java
+++ src/main/java/com/dio/e_commerce/config/CloudinaryConfig.java
@@ -2,20 +2,21 @@ package com.dio.e_commerce.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")
-        ));
+        Map<String, String> config = ObjectUtils.asMap(
+                "cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"),
+                "api_key", System.getenv("CLOUDINARY_API_KEY"),
+                "api_secret", System.getenv("CLOUDINARY_API_SECRET")
+        );
+        return new Cloudinary(config);
     }
 }
